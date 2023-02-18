ec17efc171d0303f51c085e0f5acf9cb03533442
MDKamrulHaque <kamrulhaque@live.com>
2023-02-18 16:56:50 +0000

    ATMMachine with functions to Deposit and Transfer funds is complete with Max 3 Attepnts at log in function added

--- ATMMachine/Functions.cs
+++ ATMMachine/Functions.cs
@@ -11,45 +11,44 @@ namespace ATMMachine
     {
        internal static void DepositMoney(Account userAccount)
         {
-            Console.Clear();
-            Console.WriteLine("\n---------Deposit Service---------\n");
-            Console.WriteLine($"\nhow much would you like to deposit\n");
-            decimal userDepositAmount = Convert.ToDecimal(Console.ReadLine());
-            if (userDepositAmount <= 5)
+            try
             {
-               
-                Console.WriteLine("\nInvalid, minimum amount must be £5");
-                Console.WriteLine("\nTo go back to main menu press any key");
-                Console.ReadLine();
                 Console.Clear();
-            }
-            else if (userDepositAmount > 500)
-            {
-                
-                Console.WriteLine("\nDeposit amount cannot be more than £500\n");
-                Console.WriteLine("\nTo go back to main menu press any key");
-                Console.ReadLine();
-                Console.Clear();
-            }
-            else if(userDepositAmount <= 500)
-            {
-                userAccount.AccountBalance += userDepositAmount;
-               
-                Console.WriteLine($"\nYou Deposited £{userDepositAmount}. \n\nYour new balance is £{userAccount.AccountBalance}\n");
-                Console.WriteLine("\nTo go back to main menu press any key");
-                Console.ReadLine();
-                Console.Clear();
-            }
-            else
-            {
-                Console.WriteLine("\nError: somthing went wrong please try again.\n");
-                Console.WriteLine("\nTo go back to main menu press any key");
-                Console.ReadLine();
-                Console.Clear();
+                Console.WriteLine("\n---------Deposit Service---------\n");
+                Console.WriteLine($"\nhow much would you like to deposit\n");
+                decimal userDepositAmount = Convert.ToDecimal(Console.ReadLine());
+                if (userDepositAmount < 5)
+                {
+                    Console.WriteLine("\nInvalid, minimum amount must be £5");
+                    Console.WriteLine("\nTo go back to main menu press any key");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else if (userDepositAmount > 500)
+                {
+                    Console.WriteLine("\nDeposit amount cannot be more than £500\n");
+                    Console.WriteLine("\nTo go back to main menu press any key");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else 
+                {
+                    userAccount.AccountBalance += userDepositAmount;
 
+                    Console.WriteLine($"\nYou Deposited £{userDepositAmount}. \n\nYour new balance is £{userAccount.AccountBalance}\n");
+                    Console.WriteLine("\nTo go back to main menu press any key");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
             }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nError: only numeric values are allowed, please try again.\n");
+                Console.WriteLine("\nTo go back to main menu press any key");
+                Console.ReadLine();
+                Console.Clear();
+            }           
         }
-
         internal static void ViewBalance(Account userAccount)
         {
             Console.Clear();
@@ -59,93 +58,127 @@ namespace ATMMachine
             Console.ReadLine();
             Console.Clear();
         }
-
         internal static void WithDrawMoney(Account userAccount)
         {
-            Console.Clear();
-            Console.WriteLine("\n---------Withdraw Service---------\n");
-            Console.WriteLine($"\nhow much would you like to take out\n");
-            decimal userWithDrawAmount = Convert.ToDecimal(Console.ReadLine());
-            if (userWithDrawAmount <= 5)
+            try
             {
-                
-                Console.WriteLine("\nInvalid, minimum amount must be £5");
-                Console.WriteLine("\nTo go back to main menu press any key");
-                Console.ReadLine();
                 Console.Clear();
-            }
-            else if(userWithDrawAmount > 500 || userWithDrawAmount> userAccount.AccountBalance)
-            {
-                
-                Console.WriteLine("\nNot possible! amount cannot exceed account balance or be more than £500\n");
-                Console.WriteLine("\nTo go back to main menu press any key");
-                Console.ReadLine();
-                Console.Clear();
-            }
-            else if(userWithDrawAmount <= 500)
-            {
-                userAccount.AccountBalance -= userWithDrawAmount;
-                Console.WriteLine($"\nYou toke out £{userWithDrawAmount}. Your new balance is £{userAccount.AccountBalance}\n");
-                Console.WriteLine("\nTo go back to main menu press any key");
-                Console.ReadLine();
-                Console.Clear();
-            }
-            else
-            {
-                Console.WriteLine("\nError: somthing went wrong please try again.\n");
-                Console.WriteLine("\nTo go back to main menu press any key");
-                Console.ReadLine();
-                Console.Clear();
+                Console.WriteLine("\n---------Withdraw Service---------\n");
+                Console.WriteLine($"\nhow much would you like to take out\n");
+                decimal userWithDrawAmount = Convert.ToDecimal(Console.ReadLine());
+                if (userWithDrawAmount < 5)
+                {
 
+                    Console.WriteLine("\nInvalid, minimum amount must be £5");
+                    Console.WriteLine("\nTo go back to main menu press any key");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else if (userWithDrawAmount > 500 || userWithDrawAmount > userAccount.AccountBalance)
+                {
+
+                    Console.WriteLine("\nNot possible! amount cannot exceed account balance or be more than £500\n");
+                    Console.WriteLine("\nTo go back to main menu press any key");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    userAccount.AccountBalance -= userWithDrawAmount;
+                    Console.WriteLine($"\nYou toke out £{userWithDrawAmount}. Your new balance is £{userAccount.AccountBalance}\n");
+                    Console.WriteLine("\nTo go back to main menu press any key");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nError: only numeric values are allowed, please try again.\n");
+                Console.WriteLine("\nTo go back to main menu press any key");
+                Console.ReadLine();
+                Console.Clear();
             }
         }
 
         internal static void TransferMoney(Account userAccount, ListOfAllAccounts accountList)
         {
-            Console.Clear();
-            Console.WriteLine("\n---------Transfer Service---------\n");
-            Console.WriteLine("\nplease provide the account holder's name\n");
-            string receiverAccName = Console.ReadLine();
-            Console.WriteLine($"\nNow, enter their account number\n");
-            int receiverAccNumb = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine($"\nYou entered" +
-                              $"\n\nAccount Holder Name: {receiverAccName} " +
-                              $"\nAccount Number: {receiverAccNumb} " +
-                              $"\n\ndo you wish to continue? type 'y' or 'n'\n");
-            string userYesOrNo = Console.ReadLine();
-            if (userYesOrNo.Equals("y"))
+            try
             {
-                Account AccToSendMoneyTo = accountList.accounts.FirstOrDefault(a => a.AccountNumber == receiverAccNumb && a.AccountHolderName == receiverAccName);
-               
-                if (AccToSendMoneyTo != null)
-                {
-                    Console.WriteLine($"\nHow much you would like to transfer\n");
-                    decimal userSendAmount = Convert.ToDecimal(Console.ReadLine());
+                Console.Clear();
+                Console.WriteLine("\n---------Transfer Service---------\n");
+                Console.WriteLine("\nplease provide the account holder's name\n");
+                string receiverAccName = Console.ReadLine();
+                Console.WriteLine($"\nNow, enter their account number\n");
+                int receiverAccNumb = Convert.ToInt32(Console.ReadLine());
 
-                    if (userSendAmount <= 0)
+                Console.WriteLine($"\nYou entered" +
+                                  $"\n\nAccount Holder Name: {receiverAccName} " +
+                                  $"\nAccount Number: {receiverAccNumb} " +
+                                  $"\n\ndo you wish to continue? type 'y' or 'n'\n");
+                string userYesOrNo = Console.ReadLine();
+                if (userYesOrNo.Equals("y"))
+                {
+                    Account AccToSendMoneyTo = accountList.accounts.FirstOrDefault(a => a.AccountNumber == receiverAccNumb && a.AccountHolderName == receiverAccName);
+
+                    if (AccToSendMoneyTo != null && AccToSendMoneyTo.AccountNumber != userAccount.AccountNumber)
                     {
-                        Console.WriteLine("\nInvalid amount, transfer amount must be greater than £0\n");
+                        Console.WriteLine($"\nHow much you would like to transfer\n");
+                        decimal userSendAmount = Convert.ToDecimal(Console.ReadLine());
+
+                        if (userSendAmount <= 0)
+                        {
+                            Console.WriteLine("\nInvalid amount, transfer amount must be greater than £0\n");
+                            Console.WriteLine("\nTo go back to main menu press any key");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                        else if (userSendAmount > userAccount.AccountBalance || userSendAmount > 500)
+                        {
+                            Console.WriteLine("\nNot possible! due to one of the following reason(s): " +
+                                              "\n\nTransfer amount cannot exceed account balance" +
+                                              "\nTransfer amount cannot be more than £500 \n");
+                            Console.WriteLine("\nTo go back to main menu press any key");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            userAccount.AccountBalance -= userSendAmount;
+                            AccToSendMoneyTo.AccountBalance += userSendAmount;
+                            Console.WriteLine($"\nTransaction complete, £{userSendAmount} sent to {AccToSendMoneyTo.AccountHolderName}\n");
+                            Console.WriteLine("\nTo go back to main menu press any key");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                    }
+                    else if(AccToSendMoneyTo != null && userAccount.AccountNumber == AccToSendMoneyTo.AccountNumber)
+                    {
+                        Console.WriteLine("\nError: Cannot transfer money to own amount");
                         Console.WriteLine("\nTo go back to main menu press any key");
                         Console.ReadLine();
                         Console.Clear();
                     }
-                    else if (userSendAmount > userAccount.AccountBalance || userSendAmount > 500)
+                     else
                     {
-                        Console.WriteLine("\ntransfer amount cannot exceed account balance or be more than £500\n");
+                        Console.WriteLine("\nError: Account holder's name & number doesn't match. Please try again.\n");
                         Console.WriteLine("\nTo go back to main menu press any key");
                         Console.ReadLine();
                         Console.Clear();
                     }
-                    else
-                    {
-                        userAccount.AccountBalance -= userSendAmount;
-                        AccToSendMoneyTo.AccountBalance += userSendAmount;
-                        Console.WriteLine($"\nTransaction complete, £{userSendAmount} sent to {AccToSendMoneyTo.AccountHolderName}\n");
-                        Console.WriteLine("\nTo go back to main menu press any key");
-                        Console.ReadLine();
-                        Console.Clear();
-                    }
+                }
+                else if (userYesOrNo.Equals("n"))
+                {
+                    Console.WriteLine("\nTransaction cancelled!\n");
+                    Console.WriteLine("\nTo go back to main menu press any key");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else if (!userYesOrNo.Equals("y") || !userYesOrNo.Equals("n"))
+                {
+                    Console.WriteLine("\nProcess terminated due to invalid sellection\n");
+                    Console.WriteLine("\nTo go back to main menu press any key");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
                 else
                 {
@@ -154,22 +187,14 @@ namespace ATMMachine
                     Console.ReadLine();
                     Console.Clear();
                 }
-            } else if (userYesOrNo.Equals("n"))
+            }
+            catch (FormatException)
             {
-               
-                Console.WriteLine("\nTransaction cancelled!\n");
+                Console.WriteLine("\nError: only numeric values are allowed, please try again.\n");
                 Console.WriteLine("\nTo go back to main menu press any key");
                 Console.ReadLine();
                 Console.Clear();
             }
-            else
-            {
-                Console.WriteLine("\nError: Account holder's name & number doesn't match. Please try again.\n");
-                Console.WriteLine("\nTo go back to main menu press any key");
-                Console.ReadLine();
-                Console.Clear();
-            }
-
         }
     }
 }
--- ATMMachine/Program.cs
+++ ATMMachine/Program.cs
@@ -11,95 +11,115 @@ string transfer = "TRANSFER";
 string logOut = "LOGOUT";
 
 string userInput = "";
+int maxAttempts = 3;
 
-do
-{
-    try
-    {       
-        Console.WriteLine("\nWelcome to Super ATM Service?");
-        Console.WriteLine("\nPlease Verify your details");
-        Console.WriteLine("\nEnter account number (or type 'EXIT' to quit):\n");
-        userInput = Console.ReadLine();
-        if (userInput.ToLower() == exit.ToLower())
+
+for (int i = 1; i<=maxAttempts; i++)
+    {
+        try
         {
-            break;
-        }
-
-        int userInputAccNum = Convert.ToInt32(userInput);
-
-        Console.WriteLine("\nPlease enter your pin:\n");
-        int userInputAccPin = Convert.ToInt32(Console.ReadLine());
-
-        Account userAccount = accountList.accounts.FirstOrDefault(a => a.AccountNumber == userInputAccNum && a.AccountPin == userInputAccPin);
-
-        if (userAccount != null)
-        {
-            Console.Clear();
-            string userSelectionMenuTwo = null;
-            do
+            Console.WriteLine("\nWelcome to Super ATM Service?");
+            Console.WriteLine("\nPlease Verify your details");
+            Console.WriteLine("\nEnter account number (or type 'EXIT' to quit):\n");
+            userInput = Console.ReadLine();
+              
+            if (userInput.ToLower() == exit.ToLower())
             {
-                Console.WriteLine($"\nPlease make a selection\n");
-                Console.WriteLine($"\n******************************************");
-                Console.WriteLine($"*  type 'Deposit'  to deposit money      *");
-                Console.WriteLine($"*  type 'Withdraw' to take out money     *");
-                Console.WriteLine($"*  type 'Transfer' to send moeny         *");
-                Console.WriteLine($"*  type 'Balance'  to see your balance   *");
-                Console.WriteLine($"*  type 'Logout'   to go logout          *");
-                Console.WriteLine($"******************************************\n");
-               
-                string userSelectionMenu = Console.ReadLine();
+                break;
+            }
 
-                try
+            int userInputAccNum = Convert.ToInt32(userInput);
+
+            Console.WriteLine("\nPlease enter your pin:\n");
+            int userInputAccPin = Convert.ToInt32(Console.ReadLine());
+
+            Account userAccount = accountList.accounts.FirstOrDefault(a => a.AccountNumber == userInputAccNum && a.AccountPin == userInputAccPin);
+
+            if (userAccount != null)
+            {
+                Console.Clear();
+                string userSelectionMenuTwo = null;
+                Console.WriteLine($"\nWelcome {userAccount.AccountHolderName}, Hope your having a wonderful day! (>_<)\n");
+                do
                 {
-                    if (userSelectionMenu.Equals(withDraw, StringComparison.OrdinalIgnoreCase))
-                    {
+                    Console.WriteLine($"\nPlease make a selection\n");
+                    Console.WriteLine($"\n******************************************");
+                    Console.WriteLine($"*  type 'Deposit'  to deposit money      *");
+                    Console.WriteLine($"*  type 'Withdraw' to take out money     *");
+                    Console.WriteLine($"*  type 'Transfer' to send moeny         *");
+                    Console.WriteLine($"*  type 'Balance'  to see your balance   *");
+                    Console.WriteLine($"*  type 'Logout'   to go logout          *");
+                    Console.WriteLine($"******************************************\n");
 
-                        Functions.WithDrawMoney(userAccount);
+                    string userSelectionMenu = Console.ReadLine();
 
-                    }
-                    else if (userSelectionMenu.Equals(deposit, StringComparison.OrdinalIgnoreCase))
+                    try
                     {
-                        Functions.DepositMoney(userAccount);
-
+                        if (userSelectionMenu.Equals(withDraw, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Functions.WithDrawMoney(userAccount);
+                        }
+                        else if (userSelectionMenu.Equals(deposit, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Functions.DepositMoney(userAccount);
+                        }
+                        else if (userSelectionMenu.Equals(viewBalance, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Functions.ViewBalance(userAccount);
+                        }
+                        else if (userSelectionMenu.Equals(transfer, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Functions.TransferMoney(userAccount, accountList);
+                        }
+                        else if (userSelectionMenu.Equals(logOut, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\ninvalid selection! please try again\n");
+                        }
                     }
-                    else if (userSelectionMenu.Equals(viewBalance, StringComparison.OrdinalIgnoreCase))
-                    {
-                        Functions.ViewBalance(userAccount);
-                    }
-                    else if (userSelectionMenu.Equals(transfer, StringComparison.OrdinalIgnoreCase))
-                    {
-                        Functions.TransferMoney(userAccount, accountList);
-                    }
-                    else if (userSelectionMenu.Equals(logOut, StringComparison.OrdinalIgnoreCase))
+                    catch (FormatException)
                     {
                         Console.Clear();
-                        break;
+                        Console.WriteLine("\ninvalid, use numbers\n");
                     }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("\ninvalid selection! please try again\n");
-                    }
-                }
-                catch (FormatException)
+
+                } while (userSelectionMenuTwo != logOut);
+            }
+            else
+            {
+                if (i == maxAttempts)
                 {
                     Console.Clear();
-                    Console.WriteLine("\ninvalid, use numbers\n");
+                    Console.WriteLine("\nprocess terminated due to failing to verify account details:\n");
+                    Environment.Exit(0);
                 }
-
-            } while (userSelectionMenuTwo != logOut);
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine($"\nInvalid account number or pin. Please try again");
+                    Console.WriteLine($"\nYou {maxAttempts - i} more attempt(s) left or process will be terminated\n");
+                }
+            }
         }
-        else
+        catch (FormatException)
         {
-            Console.Clear();
-            Console.WriteLine("\nInvalid account number or pin. Please try again.\n");
+                if (i == maxAttempts)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nprocess terminated due to failing to verify account details:\n");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nError: Account Number & Pin must in forms of numeric values \n");
+                    Console.WriteLine($"\nYou {maxAttempts - i} more attempt(s) left or process will be terminated\n");
+                }      
         }
     }
-    catch(FormatException)
-    {
-        Console.Clear();
-        Console.WriteLine("\nError: please try again using numbers\n");
-        
-    }
-  
-} while (userInput != exit);
+    
